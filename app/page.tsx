17d06853b1839494710f071ad579e84b5d6985e2
Stanislau <willstesi@gmail.com>
2024-06-15 21:31:58 +0300

    feat: connect screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import FirstScreen from "@/screens/first-screen"
+import SecondScreen from "@/screens/second-screen"
+
 export default function Home() {
-  return <>Main</>
+  return (
+    <div className="flex flex-col">
+      <FirstScreen />
+      <SecondScreen />
+    </div>
+  )
 }
