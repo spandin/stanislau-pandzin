1122ab4313de6928f5dec2bee61b0ade6c14168c
Stanislau <willstesi@gmail.com>
2024-06-15 21:32:48 +0300

    feat: remove navbar

--- widgets/navbar.tsx
+++
@@ -1,9 +0,0 @@
-import { Navbar as NextUINavbar } from "@nextui-org/navbar"
-
-export const Navbar = () => {
-  return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      Navbar
-    </NextUINavbar>
-  )
-}
