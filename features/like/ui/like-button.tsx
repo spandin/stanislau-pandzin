82fa01a486032b80627346d593cd7d78ad88a46a
Stanislau <willstesi@gmail.com>
2024-06-16 20:34:24 +0300

    feat: create likje button

---
+++ features/like/ui/like-button.tsx
@@ -0,0 +1,143 @@
+"use client"
+import { useEffect, useState } from "react"
+import { Button, useDisclosure } from "@nextui-org/react"
+import { Heart } from "lucide-react"
+import { AnimatePresence, motion } from "framer-motion"
+
+import EmailModal from "./email-modal"
+
+import {
+  arrayRemove,
+  arrayUnion,
+  db,
+  doc,
+  getDoc,
+  increment,
+  updateDoc,
+} from "@/shared/config/firebase"
+import { useLikeStore } from "@/app/store"
+
+export function LikeButton() {
+  const [count, setCount] = useState(0)
+  const email = useLikeStore((state) => state.email)
+  const isLike = useLikeStore((state) => state.isLike)
+  const setIsLike = useLikeStore((state) => state.setIsLike)
+
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure()
+
+  const handleLike = async () => {
+    if (!email) {
+      onOpen()
+    }
+
+    const likeDocRef = doc(db, "likes", "likeData")
+    const likeDoc = await getDoc(likeDocRef)
+
+    if (likeDoc.exists()) {
+      const data = likeDoc.data()
+      const userAlreadyLiked = data.emails.some(
+        (entry: { email: string }) => entry.email === email,
+      )
+
+      if (!userAlreadyLiked && !!email) {
+        await updateDoc(likeDocRef, {
+          likes: increment(1),
+          emails: arrayUnion({ email }),
+        })
+
+        setIsLike(true)
+        setCount((prev) => prev + 1)
+      }
+
+      if (userAlreadyLiked) {
+        await updateDoc(likeDocRef, {
+          likes: increment(-1),
+          emails: arrayRemove({ email }),
+        })
+
+        setIsLike(false)
+        setCount((prev) => prev - 1)
+      }
+    }
+  }
+
+  useEffect(() => {
+    const getLikesCount = async () => {
+      const likeDocRef = doc(db, "likes", "likeData")
+      const docRef = await getDoc(likeDocRef)
+
+      setCount(docRef?.data()?.likes)
+    }
+
+    return () => {
+      getLikesCount()
+    }
+  }, [setCount])
+
+  return (
+    <div className="absolute inset-x-0 bottom-10 flex flex-col justify-center items-center gap-2 font-semibold z-30">
+      <AnimatePresence mode="wait">
+        {isLike ? (
+          <motion.div
+            key="heart-filled"
+            initial={{ opacity: 0, rotate: 90 }}
+            animate={{ opacity: 1, rotate: 0 }}
+            exit={{ opacity: 0, rotate: -180 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Button
+              className="rounded-full"
+              isIconOnly
+              color="danger"
+              aria-label="Like"
+              onClick={handleLike}
+            >
+              <Heart fill={"white"} color="white" />
+            </Button>
+          </motion.div>
+        ) : (
+          <motion.div
+            key="heart-solid"
+            initial={{ opacity: 0, rotate: 90 }}
+            animate={{ opacity: 1, rotate: 0 }}
+            exit={{ opacity: 0, rotate: 180 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Button
+              className="rounded-full"
+              isIconOnly
+              color="danger"
+              aria-label="Like"
+              onClick={handleLike}
+            >
+              <Heart fill={"transparent"} color="white" />
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <div className="flex flex-row justify-center items-center gap-2 font-mono">
+        <AnimatePresence mode="wait">
+          <motion.span
+            key="heart-count"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+            className="text-xl"
+          >
+            {count}
+          </motion.span>
+        </AnimatePresence>
+
+        <span className="text-xl">Likes</span>
+      </div>
+
+      <EmailModal
+        isOpen={isOpen}
+        onClose={onClose}
+        onOpenChange={onOpenChange}
+      />
+    </div>
+  )
+}
