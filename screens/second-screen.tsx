d6ef6871f60a2f1cfdf9f6e0e6349f44ec802bab
Stanislau <willstesi@gmail.com>
2024-06-16 15:06:28 +0300

    feat: create screen

--- screens/second-screen.tsx
+++ screens/second-screen.tsx
@@ -1,26 +1,76 @@
-"use client"
-
-import { motion } from "framer-motion"
-import { useInView } from "react-intersection-observer"
+import TypingText from "@/shared/ui/typing-text"
 
 export default function SecondScreen() {
-  const { ref, inView } = useInView({
-    triggerOnce: false, // Запускать только один раз при появлении в зоне видимости
-    threshold: 0, // Порог видимости: 0 - при появлении полностью в зоне видимости
-  })
-
   return (
-    <div className="relative h-screen flex flex-col">
-      <div className="h-full m-auto inline-flex flex-col flex-shrink items-center justify-center ">
-        <motion.h1
-          ref={ref}
-          className="text-[96px] text-wrap font-semibold leading-[0.8]"
-          initial={{ opacity: 0, x: -300 }}
-          animate={{ opacity: 1, x: inView ? 0 : -300 }}
-          transition={{ duration: 0.4 }}
-        >
-          Second <span className="text-primary">Screen</span>
-        </motion.h1>
+    <div className="relative flex flex-col">
+      <div className="container h-full max-w-7xl mx-auto py-36 inline-flex flex-col flex-shrink items-start justify-start gap-20">
+        <TypingText
+          className="self-start text-7xl font-semibold leading-[1]"
+          containerAnimation={{
+            hidden: {
+              x: -50,
+            },
+            visible: {
+              x: { inView: 20, defaultValue: 0 },
+            },
+          }}
+          words={[
+            { word: "I", style: "white" },
+            { word: "have", style: "white" },
+            { word: "experience ", style: "text-primary" },
+            { word: "with:", style: "text-yellow-500" },
+          ]}
+        />
+
+        <TypingText
+          className="self-end text-6xl font-semibold leading-[1]"
+          containerAnimation={{
+            hidden: {
+              x: 50,
+            },
+            visible: {
+              x: { inView: -20, defaultValue: 0 },
+            },
+          }}
+          words={[
+            { word: "React,", style: "text-blue-500" },
+            { word: "Next.js " },
+          ]}
+        />
+
+        <TypingText
+          className="self-start text-5xl font-semibold leading-[1]"
+          containerAnimation={{
+            hidden: {
+              x: -50,
+            },
+            visible: {
+              x: { inView: 20, defaultValue: 0 },
+            },
+          }}
+          words={[
+            { word: "Redux,", style: "text-purple-500" },
+            { word: "Zustand " },
+          ]}
+        />
+
+        <TypingText
+          className="self-end text-4xl text-wrap text-right font-semibold leading-[1]"
+          containerAnimation={{
+            hidden: {
+              x: 20,
+            },
+            visible: {
+              x: { inView: -20, defaultValue: 0 },
+            },
+          }}
+          words={[
+            { word: "Chakra UI," },
+            { word: "Mantine,", style: "text-yellow-500" },
+            { word: "and" },
+            { word: "Next UI", style: "text-primary" },
+          ]}
+        />
       </div>
     </div>
   )
