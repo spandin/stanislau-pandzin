4c03e9593d372a8862bef70cc429f0ee77099cf7
Stanislau <willstesi@gmail.com>
2024-06-16 16:30:05 +0300

    feat: update

--- screens/second-screen.tsx
+++ screens/second-screen.tsx
@@ -2,7 +2,7 @@ import TypingText from "@/shared/ui/typing-text"
 
 export default function SecondScreen() {
   return (
-    <div className="relative h-screen flex flex-col">
+    <div className="relative flex flex-col">
       <div className="container max-w-7xl mx-auto py-20 flex flex-col gap-10  ">
         <TypingText
           className="self-start text-7xl font-semibold leading-[1]"
@@ -15,8 +15,7 @@ export default function SecondScreen() {
             },
           }}
           words={[
-            { word: "I", style: "white" },
-            { word: "have", style: "white" },
+            { word: "I have", style: "white" },
             { word: "experience", style: "text-primary" },
             { word: "with:", style: "text-yellow-500" },
           ]}
@@ -32,14 +31,11 @@ export default function SecondScreen() {
               x: { inView: -20, defaultValue: 0 },
             },
           }}
-          words={[
-            { word: "React", style: "text-blue-500" },
-            { word: "Next.js " },
-          ]}
+          words={[{ word: "React/Next.js" }]}
         />
 
         <TypingText
-          className="self-start text-5xl font-medium leading-[1]"
+          className="self-start text-5xl font-semibold leading-[1]"
           containerAnimation={{
             hidden: {
               x: -50,
@@ -48,14 +44,11 @@ export default function SecondScreen() {
               x: { inView: 20, defaultValue: 0 },
             },
           }}
-          words={[
-            { word: "Redux", style: "text-purple-500" },
-            { word: "Zustand " },
-          ]}
+          words={[{ word: "Redux/Zustand" }]}
         />
 
         <TypingText
-          className="self-end text-4xl text-wrap text-right font-normal leading-[1]"
+          className="self-end text-3xl text-wrap text-right font-semibold leading-[1]"
           containerAnimation={{
             hidden: {
               x: 20,
@@ -65,10 +58,8 @@ export default function SecondScreen() {
             },
           }}
           words={[
-            { word: "Chakra UI" },
-            { word: "Next UI", style: "text-primary" },
-            { word: "and" },
-            { word: "Framer motion", style: "text-[#f32078] " },
+            { word: "Chakra UI/Next UI" },
+            { word: "Tailwind/Framer motion", style: "text-[#f32078] " },
           ]}
         />
       </div>
